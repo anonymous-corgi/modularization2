12ff2adadb8bd2c7ffeb36b571c984e0c111dd85
Corgi <github.com@ny13corgi.com>
2025-05-05 16:08:29 -0700

    Bump compileSdk and jvmTarget

--- v2/app/build.gradle.kts
+++ v2/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
   namespace = "com.anonymouscorgi.modularization"
-  compileSdk = 34
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "com.anonymouscorgi.modularization"
@@ -27,11 +27,11 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
   }
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "11"
   }
   buildFeatures {
     compose = true
--- v2/modularization/component/clock/api/build.gradle.kts
+++ v2/modularization/component/clock/api/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
   namespace = "com.anonymouscorgi.modularization.component.clock.api"
-  compileSdk = 34
+  compileSdk = 35
 
   defaultConfig {
     minSdk = 24
@@ -21,11 +21,11 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
   }
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "11"
   }
 }
 
